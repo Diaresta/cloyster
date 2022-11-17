057846ba3b63e929624072b1ae64df4cec54ae6f
Andrew Robles <TheAndrewRobles@yahoo.com>
2022-11-16 23:17:04 -0500

    Rudimentary search styling

--- src/pages/teambuild/TeamSearch.tsx
+++ src/pages/teambuild/TeamSearch.tsx
@@ -3,13 +3,38 @@ import { useState } from 'react';
 
 const TeamSearch: NextComponentType = (props: any) => {
   return (
-    <div className='flex w-full items-center justify-center pt-6 text-2xl text-blue-500'>
+    <div className='flex w-full flex-col items-center justify-center pt-6'>
+      <div>
+        <input placeholder='asdaksdjb'></input>
+      </div>
       <ul>
         {props.pokemonsList.map((mon: any, i: number) => (
-          <div>
-            <li key={i}>{mon.name}</li>
-            <img src={mon.sprite} />
-            <img src={mon.icon} />
+          <div className='flex flex-row'>
+            <div className='flex flex-row space-x-3 space-y-3'>
+              <li>
+                <img src={mon.icon} />
+              </li>
+              <li>{mon.name}</li>
+            </div>
+            <div className='flex flex-row space-x-1'>
+              {mon.type?.map((types: any) => (
+                <li>
+                  <img src={`/images/types/${types}.png`} />
+                </li>
+              ))}
+            </div>
+            <div className='flex flex-row space-x-3'>
+              {Object.keys(mon.base).map((keyName, i) => (
+                <div>
+                  <li>
+                    <p>{keyName}</p>
+                  </li>
+                  <li>
+                    <p>{mon.base[keyName]}</p>
+                  </li>
+                </div>
+              ))}
+            </div>
           </div>
         ))}
       </ul>
