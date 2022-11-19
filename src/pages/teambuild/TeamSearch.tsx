0b3b28929447474ed15cf45d389e3c5345dc9d4d
Andrew Robles <TheAndrewRobles@yahoo.com>
2022-11-18 23:22:09 -0500

    Updating pokemon search component styling

--- src/pages/teambuild/TeamSearch.tsx
+++ src/pages/teambuild/TeamSearch.tsx
@@ -11,24 +11,26 @@ const TeamSearch: NextComponentType = (props: any) => {
       </div>
       <ul>
         {props.pokemonsList.map((mon: any, i: number) => (
-          <div className='flex flex-row'>
-            <div className='flex flex-row space-x-3 space-y-3'>
-              <li>
+          <div className='flex flex-row space-x-3'>
+            <div className='mx-auto w-1/4'>
+              <li className='flex justify-end'>
                 <img src={mon.icon} />
               </li>
+            </div>
+            <div className='mx-auto w-1/4 space-x-3 space-y-3 '>
               <li>{mon.name}</li>
             </div>
-            <div className='flex flex-row space-x-1'>
+            <div className='mx-auto flex w-1/4 flex-row space-x-1'>
               {mon.type?.map((types: any) => (
                 <li>
                   <img src={`/images/types/${types}.png`} />
                 </li>
               ))}
             </div>
-            <div className='flex flex-row space-x-3'>
+            <div className='mx-auto flex w-1/4 flex-row space-x-3'>
               {Object.keys(mon.base).map((keyName, i) => (
                 <li key={i}>
-                  <p>{STATS[i]}</p>
+                  <p className='font-medium'>{STATS[i]}</p>
                   <p>{mon.base[keyName]}</p>
                 </li>
               ))}
