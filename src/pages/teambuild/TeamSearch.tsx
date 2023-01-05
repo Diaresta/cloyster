6f2369c3290babcea2c483bca98279004804a85f
Andrew Robles <TheAndrewRobles@yahoo.com>
2023-01-04 22:49:30 -0500

    Switching search component card

--- src/pages/teambuild/TeamSearch.tsx
+++ src/pages/teambuild/TeamSearch.tsx
@@ -8,6 +8,7 @@ import { formatBase } from './TeamBuilder';
 const STATS: string[] = ['HP', 'ATK', 'DEF', 'SPC', 'SPD'];
 
 const TeamSearch: NextComponentType = (props: any) => {
+  const [searchMon, setSearchMon] = useState('');
   const [currMon, setCurrMon] = useState<pokemonProps>({
     id: 0,
     name: '',
@@ -36,130 +37,131 @@ const TeamSearch: NextComponentType = (props: any) => {
         icon={currMon.icon}
       /> */}
 
-      <div className='flex w-full justify-between'>
-        {/* <h2>{name}</h2> */}
-        <div className='flex flex-col text-left'>
-          <label
-            className='justify-start text-xs font-bold uppercase tracking-wide'
-            htmlFor='pkmn-name'
-          >
-            Pokémon
-          </label>
-          <input
-            type='text'
-            name='pkmn-name'
-            id='pkmn-name'
-            placeholder='Pokémon name...'
-            className='w-5/6 rounded border-2 border-gray-300 hover:border-gray-400 hover:opacity-80 focus:border-gray-500 sm:text-sm'
-            onFocus={() => {}}
-            required
-          />
+      <section className='flex w-full flex-col justify-center rounded border-2 border-gray-500 p-2 shadow-xl'>
+        <div className='flex w-full justify-between'>
+          {/* <div className='flex flex-col text-left'>
+            <label
+              className='justify-start text-xs font-bold uppercase tracking-wide'
+              htmlFor='pkmn-name'
+            >
+              Pokémon
+            </label>
+            <input
+              type='text'
+              name='pkmn-name'
+              id='pkmn-name'
+              placeholder='Pokémon name...'
+              className='w-5/6 rounded border-2 border-gray-300 hover:border-gray-400 hover:opacity-80 focus:border-gray-500 sm:text-sm'
+              onFocus={() => {}}
+              required
+            />
+          </div>
+          <div className='flex flex-col text-left'>
+            <label
+              className='justify-start text-xs font-bold uppercase tracking-wide'
+              htmlFor='pkmn-type'
+            >
+              Type
+            </label>
+            <ul
+              id='pkmn-type'
+              className='flex flex-row justify-evenly space-x-1 text-xs font-bold uppercase tracking-wide'
+            >
+              {currMon.type?.map((type, i) => (
+                <li key={i}>
+                  <img src={`/images/types/${type}.png`} />
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className='flex flex-col text-left'>
+            <label
+              className='justify-start text-xs font-bold uppercase tracking-wide'
+              htmlFor='pkmn-level'
+            >
+              Level
+            </label>
+            <input
+              type='text'
+              name='pkmn-level'
+              id='pkmn-level'
+              placeholder='Pokémon Level...'
+              value='100'
+              className='w-5/6 rounded border-2 border-gray-300 hover:border-gray-400 hover:opacity-80 focus:border-gray-500 sm:text-sm'
+              required
+            />
+          </div>
         </div>
-        <div className='flex flex-col text-left'>
-          <label
-            className='justify-start text-xs font-bold uppercase tracking-wide'
-            htmlFor='pkmn-type'
-          >
-            Type
-          </label>
-          <ul
-            id='pkmn-type'
-            className='flex flex-row justify-evenly space-x-1 text-xs font-bold uppercase tracking-wide'
-          >
-            {currMon.type?.map((type, i) => (
-              <li key={i}>
-                <img src={`/images/types/${type}.png`} />
+        <div className='flex flex-row justify-evenly'>
+          <div className='w-full'>
+            <img src={currMon.sprite} className='my-auto drop-shadow-lg' />
+          </div>
+          <div className='w-full text-left'>
+            <ul>
+              <li>
+                <input
+                  type='text'
+                  name='move1'
+                  className='mb-1 w-5/6 rounded border-2 border-gray-300 hover:border-gray-400 hover:opacity-80 focus:border-gray-500 sm:text-sm'
+                  placeholder='Move 1...'
+                  onSubmit={() => {}}
+                  required
+                />
               </li>
-            ))}
-          </ul>
-        </div>
-        <div className='flex flex-col text-left'>
-          <label
-            className='justify-start text-xs font-bold uppercase tracking-wide'
-            htmlFor='pkmn-level'
-          >
-            Level
-          </label>
-          <input
-            type='text'
-            name='pkmn-level'
-            id='pkmn-level'
-            placeholder='Pokémon Level...'
-            value='100'
-            className='w-5/6 rounded border-2 border-gray-300 hover:border-gray-400 hover:opacity-80 focus:border-gray-500 sm:text-sm'
-            required
-          />
-        </div>
-      </div>
-      <div className='flex flex-row justify-evenly'>
-        <div className='w-full'>
-          <img src={currMon.sprite} className='my-auto drop-shadow-lg' />
-        </div>
-        <div className='w-full text-left'>
-          <ul>
-            <li>
-              <input
-                type='text'
-                name='move1'
-                className='mb-1 w-5/6 rounded border-2 border-gray-300 hover:border-gray-400 hover:opacity-80 focus:border-gray-500 sm:text-sm'
-                placeholder='Move 1...'
-                onSubmit={() => {}}
-                required
-              />
-            </li>
-            <li>
-              <input
-                type='text'
-                name='move2'
-                className='mb-1 w-5/6 rounded border-2 border-gray-300 hover:border-gray-400 hover:opacity-80 focus:border-gray-500 sm:text-sm'
-                placeholder='Move 2...'
-                onSubmit={() => {}}
-                required
-              />
-            </li>
-            <li>
-              <input
-                type='text'
-                name='move3'
-                className='mb-1 w-5/6 rounded border-2 border-gray-300 hover:border-gray-400 hover:opacity-80 focus:border-gray-500 sm:text-sm'
-                placeholder='Move 3...'
-                onSubmit={() => {}}
-                required
-              />
-            </li>
-            <li>
-              <input
-                type='text'
-                name='move4'
-                className='mb-1 w-5/6 rounded border-2 border-gray-300 hover:border-gray-400 hover:opacity-80 focus:border-gray-500 sm:text-sm'
-                placeholder='Move 4...'
-                onSubmit={() => {}}
-                required
-              />
-            </li>
-          </ul>
-        </div>
-        <div className='flex w-full flex-row text-sm font-bold'>
-          <ul className='my-auto w-full'>
-            {Object.keys(currMon.base).map((keyName, i) => (
-              <div className='flex flex-row justify-start space-x-3'>
-                <div className='w-full'>
-                  <li key={i}>{keyName}</li>
+              <li>
+                <input
+                  type='text'
+                  name='move2'
+                  className='mb-1 w-5/6 rounded border-2 border-gray-300 hover:border-gray-400 hover:opacity-80 focus:border-gray-500 sm:text-sm'
+                  placeholder='Move 2...'
+                  onSubmit={() => {}}
+                  required
+                />
+              </li>
+              <li>
+                <input
+                  type='text'
+                  name='move3'
+                  className='mb-1 w-5/6 rounded border-2 border-gray-300 hover:border-gray-400 hover:opacity-80 focus:border-gray-500 sm:text-sm'
+                  placeholder='Move 3...'
+                  onSubmit={() => {}}
+                  required
+                />
+              </li>
+              <li>
+                <input
+                  type='text'
+                  name='move4'
+                  className='mb-1 w-5/6 rounded border-2 border-gray-300 hover:border-gray-400 hover:opacity-80 focus:border-gray-500 sm:text-sm'
+                  placeholder='Move 4...'
+                  onSubmit={() => {}}
+                  required
+                />
+              </li>
+            </ul>
+          </div>
+          <div className='flex w-full flex-row text-sm font-bold'>
+            <ul className='my-auto w-full'>
+              {Object.keys(currMon.base).map((keyName, i) => (
+                <div className='flex flex-row justify-start space-x-3'>
+                  <div className='w-full'>
+                    <li key={i}>{keyName}</li>
+                  </div>
+                  <div className='w-full'>
+                    <li>{currMon.base[keyName]}</li>
+                  </div>
+                  <div className='flex w-full'>
+                    <span
+                      className='my-auto flex h-2.5 items-start rounded-full bg-blue-600'
+                      style={{ width: formatBase(currMon.base[keyName]) }}
+                    ></span>
+                  </div>
                 </div>
-                <div className='w-full'>
-                  <li>{currMon.base[keyName]}</li>
-                </div>
-                <div className='flex w-full'>
-                  <span
-                    className='my-auto flex h-2.5 items-start rounded-full bg-blue-600'
-                    style={{ width: formatBase(currMon.base[keyName]) }}
-                  ></span>
-                </div>
-              </div>
-            ))}
-          </ul>
+              ))}
+            </ul>
+          </div> */}
         </div>
-      </div>
+      </section>
 
       <div className='mt-4 flex w-full flex-col items-center justify-center rounded border-2 border-gray-500 shadow-xl'>
         <div className='mx-auto w-full'>
